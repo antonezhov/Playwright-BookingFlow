f81edd5cc5ceb8f597cf403d663108bda6091ac8
antonezhov <anton.ezhov@joinsprouttherapy.com>
2022-01-20 14:54:17 +0300

    mobile mode is ready(optional hiden in the config)

--- playwright.config.js
+++ playwright.config.js
@@ -1,27 +1,42 @@
 // @ts-check
+const { devices } = require('@playwright/test');
 
 /** @type {import('@playwright/test').PlaywrightTestConfig} */
 const config = {
   timeout: 60000,
+  
   use: {
     viewport: { width: 1280, height: 720 },
     baseURL: 'https://stg.elemy.com',
     httpCredentials: {
       username: 'portaldev',
       password: 'portaltest123!',
-      
-
     },
-   
+
     browserName: 'chromium',
       headless: false,
+      
       launchOptions: {
-        slowMo: 500,
+        slowMo: 150,
         
         
       },
 
   },
+  projects: [
+    {
+      name: 'Desktop Chromium',
+      use: {
+        browserName: 'chromium',
+      },
+    },
+
+    // Test against mobile viewports.
+    // {
+    //   name: 'Mobile Safari',
+    //   use: devices['iPhone 12'],
+    // },
+  ],
 };
 
 module.exports = config;
--- tests/bookingflow.test.js
+++ tests/bookingflow.test.js
@@ -1,5 +1,4 @@
-const { chromium, expect } = require('@playwright/test')
-const { test } = require('@playwright/test')
+const { test, chromium, expect } = require('@playwright/test')
 const { HomePage } = require('../pages/HomePage.js')
 const { BookingPage } = require('../pages/BookingPage.js')
 
@@ -7,7 +6,7 @@ const { BookingPage } = require('../pages/BookingPage.js')
 test('Basic Positive Smoke', async ({ page }) => {
   const homepage = new HomePage(page)
   const bookingflow = new BookingPage(page)
-
+  
   await homepage.open()
   await homepage.zipCode('12345')
   await homepage.getStarted()
@@ -38,92 +37,92 @@ test('Basic Positive Smoke', async ({ page }) => {
 });
 
 
-test('Positive Smoke Shortcut Scenario', async ({ page }) => {
-  const homepage = new HomePage(page)
-  const bookingflow = new BookingPage(page)
+// test('Positive Smoke Shortcut Scenario', async ({ page }) => {
+//   const homepage = new HomePage(page)
+//   const bookingflow = new BookingPage(page)
 
-  await homepage.open()
-  await homepage.zipCode('12345')
-  await homepage.getStarted()
-  // How Old Is Your Child?
-  await bookingflow.ageSelect(bookingflow.age4_7)
-  await bookingflow.continue()
-  // Has Your Child Been Diagnosed With Autism Spectrum Disorder?
-  await bookingflow.selectOption(bookingflow.no)
-  await bookingflow.continue()
-  // Do You Have Insurance?
-  await bookingflow.selectOption(bookingflow.no)
-  await bookingflow.continue()
-  // Let's Get Started
-  await bookingflow.fillFirstName('Autotest_Short')
-  await bookingflow.fillLastName('User')
-  await bookingflow.fillEmail('autotest@email.com')
-  await bookingflow.fillPhone('(222) 222-222233')
-  await bookingflow.submit()
-  // Scedule a Call
-  // await bookingflow.clickOn(bookingflow.callScheduleLocator)
-  await bookingflow.clickOn(bookingflow.timeSlotLocator0)
-  await bookingflow.continue()
-  await bookingflow.confirm()
+//   await homepage.open()
+//   await homepage.zipCode('12345')
+//   await homepage.getStarted()
+//   // How Old Is Your Child?
+//   await bookingflow.ageSelect(bookingflow.age4_7)
+//   await bookingflow.continue()
+//   // Has Your Child Been Diagnosed With Autism Spectrum Disorder?
+//   await bookingflow.selectOption(bookingflow.no)
+//   await bookingflow.continue()
+//   // Do You Have Insurance?
+//   await bookingflow.selectOption(bookingflow.no)
+//   await bookingflow.continue()
+//   // Let's Get Started
+//   await bookingflow.fillFirstName('Autotest_Short')
+//   await bookingflow.fillLastName('User')
+//   await bookingflow.fillEmail('autotest@email.com')
+//   await bookingflow.fillPhone('(222) 222-222233')
+//   await bookingflow.submit()
+//   // Scedule a Call
+//   // await bookingflow.clickOn(bookingflow.callScheduleLocator)
+//   await bookingflow.clickOn(bookingflow.timeSlotLocator0)
+//   await bookingflow.continue()
+//   await bookingflow.confirm()
 
-});
+// });
 
-test('Unqualified by Age Scenario', async ({ page }) => {
-  const homepage = new HomePage(page)
-  const bookingflow = new BookingPage(page)
+// test('Unqualified by Age Scenario', async ({ page }) => {
+//   const homepage = new HomePage(page)
+//   const bookingflow = new BookingPage(page)
 
-  await homepage.open()
-  await homepage.zipCode('12345')
-  await homepage.getStarted()
-  // How Old Is Your Child?
-  await bookingflow.ageSelect(bookingflow.age10)
-  await bookingflow.continue()
-  // Has Your Child Been Diagnosed With Autism Spectrum Disorder?
-  await bookingflow.selectOption(bookingflow.yes)
-  await bookingflow.continue();
-  // Do You Have Insurance?
-  await bookingflow.selectOption(bookingflow.yes)
-  await bookingflow.continue()
-  // What’s Your Insurance Provider?
-  await bookingflow.providerSelect()
-  await bookingflow.continue()
-  // Let's Get Started
-  await bookingflow.fillFirstName('Autotest_Short')
-  await bookingflow.fillLastName('User')
-  await bookingflow.fillEmail('autotest@email.com')
-  await bookingflow.fillPhone('(222) 222-222233')
-  await bookingflow.submit()
-  // Go home
-  await bookingflow.clickOn(bookingflow.btnGoToHomePage)
+//   await homepage.open()
+//   await homepage.zipCode('12345')
+//   await homepage.getStarted()
+//   // How Old Is Your Child?
+//   await bookingflow.ageSelect(bookingflow.age10)
+//   await bookingflow.continue()
+//   // Has Your Child Been Diagnosed With Autism Spectrum Disorder?
+//   await bookingflow.selectOption(bookingflow.yes)
+//   await bookingflow.continue();
+//   // Do You Have Insurance?
+//   await bookingflow.selectOption(bookingflow.yes)
+//   await bookingflow.continue()
+//   // What’s Your Insurance Provider?
+//   await bookingflow.providerSelect()
+//   await bookingflow.continue()
+//   // Let's Get Started
+//   await bookingflow.fillFirstName('Autotest_Short')
+//   await bookingflow.fillLastName('User')
+//   await bookingflow.fillEmail('autotest@email.com')
+//   await bookingflow.fillPhone('(222) 222-222233')
+//   await bookingflow.submit()
+//   // Go home
+//   await bookingflow.clickOn(bookingflow.btnGoToHomePage)
 
-});
+// });
 
-test('Unqualified by ZipCode Scenario', async ({ page }) => {
-  const homepage = new HomePage(page)
-  const bookingflow = new BookingPage(page)
+// test('Unqualified by ZipCode Scenario', async ({ page }) => {
+//   const homepage = new HomePage(page)
+//   const bookingflow = new BookingPage(page)
 
-  await homepage.open()
-  await homepage.zipCode('00000')
-  await homepage.getStarted()
-  // How Old Is Your Child?
-  await bookingflow.ageSelect(bookingflow.age8_9)
-  await bookingflow.continue()
-  // Has Your Child Been Diagnosed With Autism Spectrum Disorder?
-  await bookingflow.selectOption(bookingflow.yes)
-  await bookingflow.continue();
-  // Do You Have Insurance?
-  await bookingflow.selectOption(bookingflow.yes)
-  await bookingflow.continue()
-  // What’s Your Insurance Provider?
-  await bookingflow.providerSelect()
-  await bookingflow.continue()
-  // Let's Get Started
-  await bookingflow.fillFirstName('Autotest_Short')
-  await bookingflow.fillLastName('User')
-  await bookingflow.fillEmail('autotest@email.com')
-  await bookingflow.fillPhone('(222) 222-222233')
-  await bookingflow.submit()
-  // Scedule a Call
-  await bookingflow.clickOn(bookingflow.btnGoToHomePage)
+//   await homepage.open()
+//   await homepage.zipCode('00000')
+//   await homepage.getStarted()
+//   // How Old Is Your Child?
+//   await bookingflow.ageSelect(bookingflow.age8_9)
+//   await bookingflow.continue()
+//   // Has Your Child Been Diagnosed With Autism Spectrum Disorder?
+//   await bookingflow.selectOption(bookingflow.yes)
+//   await bookingflow.continue();
+//   // Do You Have Insurance?
+//   await bookingflow.selectOption(bookingflow.yes)
+//   await bookingflow.continue()
+//   // What’s Your Insurance Provider?
+//   await bookingflow.providerSelect()
+//   await bookingflow.continue()
+//   // Let's Get Started
+//   await bookingflow.fillFirstName('Autotest_Short')
+//   await bookingflow.fillLastName('User')
+//   await bookingflow.fillEmail('autotest@email.com')
+//   await bookingflow.fillPhone('(222) 222-222233')
+//   await bookingflow.submit()
+//   // Scedule a Call
+//   await bookingflow.clickOn(bookingflow.btnGoToHomePage)
 
-});
+// });
